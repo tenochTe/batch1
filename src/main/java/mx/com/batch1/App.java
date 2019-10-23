26b1b560c8b46f76a286cc1880cad4a8cbf52c15
Tenorio Teran Oscar Enrique <XMY3087@cb.bbvabancomer.com.mx>
2019-10-23 18:07:08 -0500

    Configuracion de beans y job

--- src/main/java/mx/com/batch1/App.java
+++ src/main/java/mx/com/batch1/App.java
@@ -6,8 +6,8 @@ package mx.com.batch1;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
 }
