5d9f033cfe827025c19cf12bf964bb35ce2b5768
XMY3087 <XMY3087@xmy3087.cb.bbvabancomer.com.mx>
2019-10-24 17:51:10 -0500

    Listener Configuration

--- src/main/java/mx/com/batch1/ListenerJobExecution.java
+++ src/main/java/mx/com/batch1/ListenerJobExecution.java
@@ -4,16 +4,34 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 
 public class ListenerJobExecution implements JobExecutionListener {
+	
+	private String filePath;
 
 	@Override
-	public void afterJob(JobExecution arg0) {
-		System.out.println("#####> Hello AFTER");
-
+	public void afterJob(JobExecution jobExecution) {
+		jobExecution
+			.getStepExecutions()
+				.stream()
+					.forEach( c ->  
+						System.out.println( "-> "+c.getStepName()+" - "+filePath )
+					);
 	}
+	
 
 	@Override
-	public void beforeJob(JobExecution arg0) {
-		System.out.println("#####> Hello BEFORE");
+	public void beforeJob(JobExecution jobExecution) {
+		System
+			.getenv()
+				.entrySet()
+					.forEach( c -> { 
+						if ( "home".equals(c.getKey().toLowerCase()) 
+								|| "userprofile".equals(c.getKey().toLowerCase()) ) {
+							filePath = c.getValue();
+						}
+					});
+	
+		System.out.println(filePath);
+			
 	}
 
 }
